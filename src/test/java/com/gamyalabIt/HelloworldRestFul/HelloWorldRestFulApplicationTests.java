f1e78a7295a1c61c7762e06b12cc538352b21942
gamya <pampapathy.gamya@gmail.com>
2022-10-20 16:09:27 +0530

    this is my frist demo for Restfull web service

---
+++ src/test/java/com/gamyalabIt/HelloworldRestFul/HelloWorldRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamyalabIt.HelloworldRestFul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
